3327ef8eda361e8cfd9669b2ce0eb4dba8a26a7b
Kira-Floris <nzafloris@gmail.com>
2023-04-07 17:26:42 +0200

    changing files for version 2

--- config/onmt-other/config_transformer.sh
+++ config/onmt-other/config_transformer.sh
@@ -3,3 +3,9 @@ onmt_build_vocab -config NMT_en_kin/config/onmt-other/config_transformer.yaml -n
 
 # train
 onmt_train -config NMT_en_kin/config/onmt-other/config_transformer.yaml
+
+# test
+onmt_translate -model NMT_en_kin/models/onmt_others/en_kin_model_transformer/en_kin_model_20000.pt -src NMT_en_kin/data/test/test-en.txt -output NMT_en_kin/temp/pred_rw.txt -gpu 0 -verbose
+
+# bleu
+sacrebleu NMT_en_kin/data/test/test-rw.txt < NMNMT_en_kin/data/test/test-rw.txt
