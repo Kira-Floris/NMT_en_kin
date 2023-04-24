f032791bf2b654d48776037595a753e12729b6d6
Kira-Floris <nzafloris@gmail.com>
2023-04-25 00:23:34 +0200

    fix bug in config transformer

--- config/onmt-other/config_transformer.sh
+++ config/onmt-other/config_transformer.sh
@@ -5,7 +5,7 @@ onmt_build_vocab -config NMT_en_kin/config/onmt-other/config_transformer.yaml -n
 onmt_train -config NMT_en_kin/config/onmt-other/config_transformer.yaml
 
 # test
-onmt_translate -model NMT_en_kin/models/onmt_others/en_kin_model_transformer/en_kin_model_150000.pt -src NMT_en_kin/data/test/test-en.txt -output NMT_en_kin/temp/pred_rw.txt -gpu 0 -verbose
+onmt_translate -model NMT_en_kin/models/onmt_others/en_kin_model_transformer/en_kin_model_step_12000.pt -src NMT_en_kin/data/test/test-en.txt -output NMT_en_kin/temp/pred_rw.txt -gpu 0 -verbose
 
 # bleu
 sacrebleu NMT_en_kin/data/test/test-rw.txt < NMT_en_kin/temp/pred_rw.txt
